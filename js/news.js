dc3bfaa7d872dc2cbc5aba41d240f5c50ea9b51c
polinasvetlinova <polipsp@hotmail.com>
2017-06-02 22:15:29 +0200

    Add files via upload

---
+++ js/news.js
@@ -0,0 +1,36 @@
+//numero de archivo
+var archivo=1;
+
+
+
+$(document).ready(function() {
+
+
+	$(window).scroll(function() {
+		if ($(document).height() - $(window).height()  == $(window).scrollTop()) {					
+			cargarNoticias();
+		}
+	});
+	
+	$("#cargar").click(function(){
+			cargarNoticias();
+	});
+	  
+
+});
+
+//metodo que si lo ejecutamos cargara más noticias si no hemos alcanzado el limite de noticias que tenemos en los ficherosJSON.
+function cargarNoticias(){
+    //metodo para leer el json
+    $.getJSON( "https://rawgit.com/polinasvetlinova/noticias/master/data/"+archivo+".json", function( jsonObject ) {
+        insertarNoticia( jsonObject );
+    });
+    archivo++;
+}
+
+
+function insertarNoticia(json){
+   $.each(json, function(i,noticia){
+$("#noticias").append("<hr><div id='imgDiv'><img class='img-thumbnail' id='imgNoticia' src="+noticia.img+"alt="+noticia.alt+"></div><div id='news' class='container' id='cuerpo'><a href='#'><h3>"+noticia.title+"</h3></a><p>"+noticia.desc+"</p><footer id='date' class='small'>"+noticia.date+"</footer></div>");
+    })
+}
